1f5648efc34115a807abbf97347dbe5153ae43eb
LucasFroner <lucasfroner_@hotmail.com>
2025-04-16 15:42:58 +0000

    Adicionado código com movimentação das peças: torre, bispo e rainha.

--- xadrez.c
+++ xadrez.c
@@ -27,6 +27,43 @@ int main() {
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
+    
+
+    //bloco de variáveis
+    int t = 1;
+    int b = 1;
+
+    //bloco de apresentação do jogo
+    printf ("Bem-vindo ao jogo de Xadrez!\n");
+
+    //mover a torre 5 casas para a direita
+    printf ("\n"); //pular linha para melhor apresentação do resultado
+    printf ("Torre, andar 5 casas para a direita.\n");
+    while (t <= 5)
+    {
+        printf ("Direita\n");
+        t++;
+    }
+    
+    //mover o bispo 5 casas na diagonal cima direita
+    printf ("\n"); //pular linha para melhor apresentação do resultado
+    printf ("Bispo, mover 5 casas na diagonal cima direita.\n");
+    do
+    {
+        printf ("Cima Direita\n");
+        b++;
+    } while (b <= 5);
+    
+    //mover a rainha 8 casas para a esquerda
+    printf ("\n"); //pular linha para melhor apresentação do resultado
+    printf ("Rainha, mover 8 casas para a esquerda.\n");
+    for (int r = 1; r <= 8; r++)
+    {
+        printf ("Esquerda\n");
+    }
+    
+
+
 
     return 0;
 }
